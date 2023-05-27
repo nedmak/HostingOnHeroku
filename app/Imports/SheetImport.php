0c9d2a0cd16e44ccd2f6e42bee1c9b8e0aafff23
Nedas Makarevičius <83786916+nedmak@users.noreply.github.com>
2023-05-27 17:46:33 +0300

    fixture stats import validation

--- app/Imports/SheetImport.php
+++ app/Imports/SheetImport.php
@@ -4,11 +4,12 @@ namespace App\Imports;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithValidation;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 use App\Models\admPlayerStas;
 
-class SheetImport implements ToModel, WithHeadingRow
+class SheetImport implements ToModel, WithValidation, WithHeadingRow
 {
     public function model(array  $row)
     {
@@ -26,5 +27,21 @@ class SheetImport implements ToModel, WithHeadingRow
             'fixtureID' => session()->get('fixture'),
             'userID' => session()->get('email'),
         ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'last_name' => 'required|string',
+            'min' => 'required|integer',
+            'shots' => 'required|integer',
+            'shots_on_goal' => 'required|integer',
+            'goals' => 'required|integer',
+            'assists' => 'required|integer',
+            'yellow' => 'required|integer',
+            'red' => 'required|integer',
+            'teamID' => 'required|integer',
+        ];
     }
 }
